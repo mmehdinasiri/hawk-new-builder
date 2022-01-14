4c24d680ae8b6345efbce4157cb8a496aecb4a26
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2022-01-15 03:21:06 +0330

    move icon css file into style folder

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -2,11 +2,11 @@ module.exports = {
   name: "tamashaIcon",
   inputDir: "./src/static/icons",
   outputDir: "./src/static/fonts/webFont",
-  // fontsUrl: "../static/fonts/webFont",
+  fontsUrl: "../fonts/webFont",
   fontTypes: ["ttf", "woff", "woff2", "eot"],
   assetTypes: ["css"],
   prefix: "icon",
   pathOptions: {
-    // css: "./src/styles/icons.css",
+    css: "./src/styles/icons.css",
   },
 };
--- src/styles/css-bundles.js
+++ src/styles/css-bundles.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 const main = [path.resolve(__dirname, "./main.sass")];
 const mobile = [path.resolve(__dirname, "./mobile.sass")];
+const icons = [path.resolve(__dirname, "./icons.css")];
 const cssEntry = {
   main,
   mobile,
+  icons,
 };
 
 exports.cssEntry = cssEntry;
